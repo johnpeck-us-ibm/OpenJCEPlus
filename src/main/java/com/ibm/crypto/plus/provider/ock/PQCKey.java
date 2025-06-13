732f280fad82dd00b18516afa3c02abb9b56c6ab
John Peck <140550562+johnpeck-us-ibm@users.noreply.github.com>
2025-06-13 12:37:05 -0500

    Add debug
    Signed-off-by: John Peck <140550562+johnpeck-us-ibm@users.noreply.github.com>

--- src/main/java/com/ibm/crypto/plus/provider/ock/PQCKey.java
+++ src/main/java/com/ibm/crypto/plus/provider/ock/PQCKey.java
@@ -36,10 +36,11 @@ public final class PQCKey implements AsymmetricKey {
             keyId = NativeInterface.MLKEY_generate(ockContext.getId(), NoDashAlg);
 
             if (keyId == 0) {   
-                throw new OCKException("OCKPQCKey.generateKeyPair: MLKEY_generate failed");
+                throw new OCKException("PQCKey.generateKeyPair: MLKEY_generate failed");
             }    
         } catch (Exception e) {
-            throw new OCKException("OCKPQCKey.generateKeyPair: Exception " + e.getCause());
+            System.out.println("exception = " + e.getMessage + e.printStackTrace);
+            throw new OCKException("PQCKey.generateKeyPair: Exception " + e.getCause());
         }
         return new PQCKey(ockContext, keyId, unobtainedKeyBytes, unobtainedKeyBytes, algName);
     }
